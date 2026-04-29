b1a8978bc499f500feeafd3e5e6ccb0315b3fb74
Satarupa Bhowmik <145781992+satarupa168@users.noreply.github.com>
2026-04-30 00:37:40 +0530

    Create App component with MapComponent integration

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapComponent from './components/MapComponent';
+import 'leaflet/dist/leaflet.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
